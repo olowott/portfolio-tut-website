c79888fe931531fe6402b689d7d0421261853925
Tope Olowookere <olowott@Topes-MacBook-Pro.local>
2020-03-11 20:42:39 +0100

    Updates

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// DOM elements
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+
+//set initial state of menu
+let showMenu = false;
+menuBtn.addEventListener('click', toggleMenu);
+
+
+//funcitons 
+function toggleMenu() {
+    if (!showMenu) {
+
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuBranding.classList.add('show');
+        menuNav.classList.add('show');
+
+        //loop through the nav-items
+        navItems.forEach(item => item.classList.add('show'));
+
+        //set Menu state
+        showMenu = true;
+
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuBranding.classList.remove('show');
+        menuNav.classList.remove('show');
+
+        //loop through the nav-items
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //set Menu state
+        showMenu = false;
+
+    }
+}
